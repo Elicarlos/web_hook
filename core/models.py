040aaaf46a3b003807548a8d0909a819a065eb22
Elicarlos <elicarlosantos_@hotmail.com>
2024-05-23 08:21:51 -0300

    adicionado o modelo cliente

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+#Exemplo
+#{'nome': 'Neela', 'email': 'Murali2014@yahoo.edu', 'status': 'recusado', 'valor': 650, 'forma_pagamento': 'pix', 'parcelas': 2}
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    status = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=100, decimal_places=2)
+    forma_pagamento = models.CharField(max_length=100)
+    parcelas = models.IntegerField()
+    
+    def __str__(self):
+        return self.nome
+
+ 
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 import json
+from . models import Cliente
 
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
@@ -31,6 +32,31 @@ def hook_receiver_view(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         print(data)
+        nome = data.get('nome')
+        email = data.get('email')
+        status = data.get('status')
+        valor = data.get('valor')
+        forma_pagamento = data.get('forma_pagamento')
+        parcelas = data.get('parcelas')
+        
+        
+        Cliente.save(nome, email, status, valor, forma_pagamento, parcelas)
+        cliente = Cliente.objects.get(email=email).first()
+        dados = {
+                "nome": cliente.nome, 
+                "email": cliente.email,  
+                "status": cliente.status,  
+                "valor": cliente.valor,
+                "forma_pagamento":  cliente.forma_pagamento,
+                "parcelas": cliente.parcelas
+        }
+        dado = json.dumps(dados)
+            
+            
+        link = "https://webhook.site/e035ba9f-7b42-4b57-a7f2-694e0a9a2f9c"
+            
+        requests.post(link, data=dado)     
+
         
     else:
         print(request.body)
